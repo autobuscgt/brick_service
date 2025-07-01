32521df5a92860547fcd6eedbd50fbea82530f80
autobuscgt <lekiaaaskya@gmail.com>
2025-07-01 22:07:44 +0300

    photos desctr

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -1,10 +1,11 @@
-import React from 'react';
-import ProductionCard from './Cards/ProductionCard';
 
+import ProductionCard from './Cards/ProductionCard';
+import default_brick from '../static/production_photos/default.png'
+import mixed_brick from '../static/production_photos/mixed.png'
 function Cards(props) {
     const cards = [
-        {id:1,image:'https://media.discordapp.net/attachments/1176202984141635747/1389680856062234755/sZqiObBgS9hvniEYPim3o9JEkoGxjD5ygSVYzeMPQcCkqEtMTCu65FlrBd9hdOtU1R3XUWKBGEmyGNwa89a0dA3D3D.png?ex=686580a7&is=68642f27&hm=3fed90e0fcd352a7323c06d1add9de8b66eac6e01dec12a6779f657c46dfc80c&=&format=webp&quality=lossless',name:'Моноцвет',color:'',price:''},
-        {id:1,image:'https://media.discordapp.net/attachments/1176202984141635747/1389680880351711353/M-i_CaeHakYVE5qIsocUrykIEHXH5GstKeAxbZpy2qtfaYO0K9oLj3kKGW3gE2Fzat-JG6-XbK5inbTkMImP_w3D3D.png?ex=686580ac&is=68642f2c&hm=c50edbb91d5fd43715c0e213f1123d193aa5ab3cb070fc5f795475289003c7e3&=&format=webp&quality=lossless',name:'Миксовый',color:'',price:''},
+        {id:1,image:default_brick,name:'Моноцвет',color:'',price:''},
+        {id:1,image:mixed_brick,name:'Миксовый',color:'',price:''},
   ]
     return (
         <div className='card-centered' id='catalog'>
--- src/components/Slider.js
+++ src/components/Slider.js
@@ -5,6 +5,9 @@ import '@splidejs/react-splide/css';
 
 import '@splidejs/react-splide/css/core';
 import production_video from '../static/background utils/video_slider.mov'
+
+import colors from '../static/banner_photos/colors.png'
+import wide_range from '../static/banner_photos/wide_range.png'
 function Slider({frst,scnd,thrd}) {
     return (
     <div className='centered'>
@@ -28,7 +31,7 @@ function Slider({frst,scnd,thrd}) {
     <SplideSlide>
         <div className='slider-container'>
             <h1>{frst}</h1>
-            <img src="https://media.discordapp.net/attachments/1176202984141635747/1389680772448784544/-dFcphn7eOdUhd2JQvPduQcVyyb-j76ImuvCWfD921B64G4vO5TL9tl5TFk12psPqrkKslkqSD09hmTK9RqF5w3D3D.png?ex=68658093&is=68642f13&hm=f0a434f4a6db2654de7cd57f40fb91da0d10a282b6546ae94978ff353ac114da&=&format=webp&quality=lossless" alt=" 1"/>
+            <img src={colors} alt=" 1"/>
         </div>
           </SplideSlide>
 
@@ -36,7 +39,7 @@ function Slider({frst,scnd,thrd}) {
     <SplideSlide>
         <div className='slider-container'>
             <h1>{scnd}</h1>
-             <img src="https://media.discordapp.net/attachments/1176202984141635747/1389680806179504240/4eVWnupS70l5lycabN17zLp1j4BoqX1A2CYET2VyM9M--MPFo8ONuu4OSiAAnbilQKB9O9-Upn9Yfjy-RmEkAw3D3D.png?ex=6865809b&is=68642f1b&hm=62391ee27f77b3e08908fbe6d857a7ce19c4f2f8336d42ff5c051eed36f2e188&=&format=webp&quality=lossless" alt=" 2"/>
+             <img src={wide_range} alt=" 2"/>
         </div>
            </SplideSlide>
 
