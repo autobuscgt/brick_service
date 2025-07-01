35b80d8799faf297837ed45dea11b15de1e0ac02
autobuscgt <lekiaaaskya@gmail.com>
2025-07-02 01:17:13 +0300

    fix calculator

--- src/pages/Calculator.js
+++ src/pages/Calculator.js
@@ -6,6 +6,7 @@ function Calculator() {
             src="https://stroy-calc.ru/widj?str=raschet-kirpicha&bgcol=fff&tcol=404040&elcol=00b7ff&dis=none" 
             width="100%" 
             height="920px" 
+            title="calculator"
             ></iframe>
         </div>
 
