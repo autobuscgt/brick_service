7430e426652b06fc711dc31d35f4a0baffad020a
autobuscgt <lekiaaaskya@gmail.com>
2025-07-01 21:57:37 +0300

    third commit

--- src/components/Cards.js
+++ src/components/Cards.js
@@ -3,8 +3,8 @@ import ProductionCard from './Cards/ProductionCard';
 
 function Cards(props) {
     const cards = [
-        {id:1,image:'https://1.downloader.disk.yandex.ru/preview/1f65785ee77cffc8e978cc314b24774545e1f54cee419c0b570862c675772cd0/inf/sZqiObBgS9hvniEYPim3o9JEkoGxjD5ygSVYzeMPQcCkqEtMTCu65FlrBd9hdOtU1R3XUWKBGEmyGNwa89a0dA%3D%3D?uid=2084450616&filename=2025-06-29%2016-46-41%20%281%29.jpeg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=2084450616&tknv=v3&size=1920x911',name:'Моноцвет',color:'',price:''},
-        {id:1,image:'https://2.downloader.disk.yandex.ru/preview/f37f8741bf3135cca0d50b586c91054bec2849f93c9951ea2f9efe647af365a5/inf/M-i_CaeHakYVE5qIsocUrykIEHXH5GstKeAxbZpy2qtfaYO0K9oLj3kKGW3gE2Fzat-JG6-XbK5inbTkMImP_w%3D%3D?uid=2084450616&filename=2025-06-29%2016-48-35%20%282%29.jpeg&disposition=inline&hash=&limit=0&content_type=image%2Fjpeg&owner_uid=2084450616&tknv=v3&size=1920x911',name:'Миксовый',color:'',price:''},
+        {id:1,image:'https://media.discordapp.net/attachments/1176202984141635747/1389680856062234755/sZqiObBgS9hvniEYPim3o9JEkoGxjD5ygSVYzeMPQcCkqEtMTCu65FlrBd9hdOtU1R3XUWKBGEmyGNwa89a0dA3D3D.png?ex=686580a7&is=68642f27&hm=3fed90e0fcd352a7323c06d1add9de8b66eac6e01dec12a6779f657c46dfc80c&=&format=webp&quality=lossless',name:'Моноцвет',color:'',price:''},
+        {id:1,image:'https://media.discordapp.net/attachments/1176202984141635747/1389680880351711353/M-i_CaeHakYVE5qIsocUrykIEHXH5GstKeAxbZpy2qtfaYO0K9oLj3kKGW3gE2Fzat-JG6-XbK5inbTkMImP_w3D3D.png?ex=686580ac&is=68642f2c&hm=c50edbb91d5fd43715c0e213f1123d193aa5ab3cb070fc5f795475289003c7e3&=&format=webp&quality=lossless',name:'Миксовый',color:'',price:''},
   ]
     return (
         <div className='card-centered' id='catalog'>
