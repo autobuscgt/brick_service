b629e7bcedc701811845522f256696ea83413beb
autobuscgt <lekiaaaskya@gmail.com>
2025-07-02 15:33:04 +0300

    mixed colors added

--- src/pages/Bricks/MultiColors.js
+++ src/pages/Bricks/MultiColors.js
@@ -1,10 +1,14 @@
 import './brickstyles.css'
-import white from '../../static/temp_photos/white.jpeg'
-import yellow from '../../static/temp_photos/yellow.jpeg'
-import emerald from '../../static/temp_photos/emerald.jpeg'
-import red from '../../static/temp_photos/red.jpeg'
-import chocolate from '../../static/temp_photos/chocolate.jpeg'
-import graphite from '../../static/temp_photos/graphite.jpeg'
+
+import mixed_chocolate from '../../static/temp_photos/mixed_chocolate.jpeg'
+import yellow from '../../static/temp_photos/mixed_yellow.jpeg'
+import mixed_green from '../../static/temp_photos/mixed_green.jpeg'
+import red from '../../static/temp_photos/mixed_redgraphite.jpeg'
+import chocolate from '../../static/temp_photos/mixed_chocolate.jpeg'
+import mixed_mars from '../../static/temp_photos/mixed_mars.jpeg'
+import dark from '../../static/temp_photos/mixed_dark.jpeg'
+import mars from '../../static/temp_photos/mixed_white_grey.jpeg'
+
 import { useNavigate, useParams } from 'react-router-dom'
 import kartinka from '../../static/colors/mixed.svg'
 import Banner from '../../components/Banner'
@@ -12,24 +16,28 @@ import { useState } from 'react'
 
 function MultiColors() {
     const size1 = (<div> 1.4 НФ (полуторный) <br/>
-                    Размер: 250*120*88 <br/>
+                    Размер: 250*120*88 <br/> Цвет:<br/>
                     Фактура (вид): гладкий </div>)
     const size2 = (<div> 1НФ (одинарный) <br/>
-                    Размер: 250*120*65 <br/>
+                    Размер: 250*120*65 <br/> Цвет:<br/>
                     Фактура (вид): гладкий</div> )
         const bricks = [
-        {id:1,name:'Белый',description:size2,price:'',image:white, type:'1 НФ'},
-        {id:2,name:'Белый',description:size1,price:'',image:white, type:'1.4 НФ'},
-        {id:3,name:'Желтый',description:size2,price:'',image:yellow, type:'1 НФ'},
-        {id:4,name:'Желтый',description:size1,price:'',image:yellow, type:'1.4 НФ'},
-        {id:5,name:'Красный',description:size2,price:'',image:red, type:'1 НФ'},
-        {id:6,name:'Красный',description:size1,price:'',image:red, type:'1.4 НФ'},
-        {id:7,name:'Графит',description:size2,price:'',image:graphite, type:'1 НФ'},
-        {id:8,name:'Графит',description:size1,price:'',image:graphite, type:'1.4 НФ'},
-        {id:9,name:'Изумруд',description:size2,price:'',image:emerald, type:'1 НФ'},
-        {id:10,name:'Изумруд',description:size1,price:'',image:emerald, type:'1.4 НФ'},
-        {id:11,name:'Шоколад',description:size2,price:'',image:chocolate, type:'1 НФ'},
-        {id:12,name:'Шоколад',description:size1,price:'',image:chocolate, type:'1.4 НФ'},
+        {id:1,name:'Шоколадная глазурь',description:size2,price:'',image:mixed_chocolate, type:'1 НФ'},
+        {id:2,name:'Шоколадная глазурь',description:size1,price:'',image:mixed_chocolate, type:'1.4 НФ'},
+        {id:3,name:'Медальон',description:size2,price:'',image:yellow, type:'1 НФ'},
+        {id:4,name:'Медальон',description:size1,price:'',image:yellow, type:'1.4 НФ'},
+        {id:5,name:'Красный графит',description:size2,price:'',image:red, type:'1 НФ'},
+        {id:6,name:'Красный графит',description:size1,price:'',image:red, type:'1.4 НФ'},
+        {id:7,name:'Марс',description:size2,price:'',image:mixed_mars, type:'1 НФ'},
+        {id:8,name:'Марс',description:size1,price:'',image:mixed_mars, type:'1.4 НФ'},
+        {id:9,name:'Фисташка',description:size2,price:'',image:mixed_green, type:'1 НФ'},
+        {id:10,name:'Фисташка',description:size1,price:'',image:mixed_green, type:'1.4 НФ'},
+        {id:11,name:'Шоколадная глазурь',description:size2,price:'',image:chocolate, type:'1 НФ'},
+        {id:12,name:'Шоколадная глазурь',description:size1,price:'',image:chocolate, type:'1.4 НФ'},
+        {id:12,name:'Чернобурка',description:size2,price:'',image:dark, type:'1 НФ'},
+        {id:13,name:'Чернобурка',description:size1,price:'',image:dark, type:'1.4 НФ'},
+        {id:14,name:'Шиншилла',description:size2,price:'',image:mars, type:'1 НФ'},
+        {id:15,name:'Шиншилла',description:size1,price:'',image:mars, type:'1.4 НФ'},
     ]
     const [filteredData, setData] = useState(bricks)
     const {id} = useParams()
